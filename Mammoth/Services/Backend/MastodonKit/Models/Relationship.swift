fd78849bbe1fdfe04aa9cd7fbb2aae9c4521b47e
Benoit Nolens <bnolens@gmail.com>
2023-12-29 16:00:34 +0100

    MAM-3623-correctly-update-follow-status-on-profile-screen (#251)

--- Mammoth/Services/Backend/MastodonKit/Models/Relationship.swift
+++ Mammoth/Services/Backend/MastodonKit/Models/Relationship.swift
@@ -8,7 +8,21 @@
 
 import Foundation
 
-public class Relationship: Codable {
+public class Relationship: Codable, Equatable {
+    public static func == (lhs: Relationship, rhs: Relationship) -> Bool {
+        return lhs.id == rhs.id &&
+        lhs.following == rhs.following &&
+        lhs.followedBy == rhs.followedBy &&
+        lhs.blocking == rhs.blocking &&
+        lhs.muting == rhs.muting &&
+        lhs.mutingNotifications == rhs.mutingNotifications &&
+        lhs.requested == rhs.requested &&
+        lhs.domainBlocking == rhs.domainBlocking &&
+        lhs.showingReblogs == rhs.showingReblogs &&
+        lhs.note == rhs.note &&
+        lhs.notifying == rhs.notifying
+    }
+    
     /// Target account id.
     public let id: String
     /// Whether the user is currently following the account.
--- Mammoth/Utils/PostActions.swift
+++ Mammoth/Utils/PostActions.swift
@@ -732,8 +732,8 @@ extension PostActions {
     }
     
     static func onProfilePress(target: UIViewController, postCard: PostCardModel) {
-        if let account = postCard.user?.account {
-            self.onProfilePress(target: target, account: account)
+        if let user = postCard.user {
+            self.onProfilePress(target: target, user: user)
         }
     }
     
@@ -741,7 +741,7 @@ extension PostActions {
         triggerHapticImpact(style: .light)
         
         let userCardModel = UserCardModel(account: account, requestFollowStatusUpdate: .whenUncertain)
-        let isSelf = account.fullAcct == AccountsManager.shared.currentUser()?.fullAcct
+        let isSelf = account.remoteFullOriginalAcct == AccountsManager.shared.currentUser()?.remoteFullOriginalAcct
         let profileVC = ProfileViewController(user: userCardModel, screenType: isSelf ? .own : .others)
         if profileVC.isBeingPresented {} else {
             target.navigationController?.pushViewController(profileVC, animated: true)
@@ -751,7 +751,7 @@ extension PostActions {
     static func onProfilePress(target: UIViewController, user: UserCardModel) {
         triggerHapticImpact(style: .light)
         
-        let isSelf = user.account?.fullAcct == AccountsManager.shared.currentUser()?.fullAcct
+        let isSelf = user.account?.remoteFullOriginalAcct == AccountsManager.shared.currentUser()?.remoteFullOriginalAcct
         let profileVC = ProfileViewController(user: user, screenType: isSelf ? .own : .others)
         if profileVC.isBeingPresented {} else {
             target.navigationController?.pushViewController(profileVC, animated: true)
