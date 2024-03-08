43052468b05a1deb98447e4aabb37611b88bf4dd
Benoit Nolens <bnolens@gmail.com>
2024-03-08 21:42:55 +0100

    MAM-4018-client-side-fallback-to-only-enable-supported-languages (#472)
    * MAM-4018-client-side-fallback-to-only-enable-supported-languages
    
    * Adding pt as a supported language
    
    * Revert previous change

--- Mammoth/SceneDelegate.swift
+++ Mammoth/SceneDelegate.swift
@@ -35,6 +35,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
+        l10n.start()
+        l10n.checkForSupportedLanguage()
+        
         NotificationCenter.default.addObserver(self, selector: #selector(self.overrideTheme), name: NSNotification.Name(rawValue: "overrideTheme"), object: nil)
         self.overrideTheme()
         
---
+++ Mammoth/Services/l10n/CrowdinService.swift
@@ -0,0 +1,37 @@
+//
+//  CrowdinService.swift
+//  Mammoth
+//
+//  Created by Benoit Nolens on 08/03/2024
+//  Copyright © 2024 The BLVD. All rights reserved.
+//
+
+import Foundation
+import CrowdinSDK
+import ArkanaKeys
+
+struct l10n {
+    public static func start() {
+        let crowdinProviderConfig = CrowdinProviderConfig(hashString: ArkanaKeys.Global().crowdinDistributionString,
+                                                          sourceLanguage: GlobalStruct.rootLocalization)
+        
+        let crowdinSDKConfig = CrowdinSDKConfig.config().with(crowdinProviderConfig: crowdinProviderConfig)
+            .with(settingsEnabled: false)
+        
+        CrowdinSDK.startWithConfig(crowdinSDKConfig, completion: { })
+    }
+    
+    public static func checkForSupportedLanguage() {
+        // Fallback to root localization if current device language is not supported
+        let supported = GlobalStruct.supportedLocalizations
+        if let currentLanguage = Locale.current.languageCode {
+            if !supported.contains(currentLanguage) {
+                CrowdinSDK.currentLocalization = GlobalStruct.rootLocalization
+            } else {
+                CrowdinSDK.currentLocalization = currentLanguage
+            }
+        } else {
+            CrowdinSDK.currentLocalization = GlobalStruct.rootLocalization
+        }
+    }
+}
