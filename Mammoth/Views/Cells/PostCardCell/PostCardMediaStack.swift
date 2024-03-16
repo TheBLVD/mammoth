0385b1af56123314ac1b83e2756ca75da376a184
piuvas <piuvas@proton.me>
2024-03-16 16:43:52 -0300

    resize AFTER creating blurhash.

--- Mammoth/Views/Cells/PostCardCell/PostCardMediaStack.swift
+++ Mammoth/Views/Cells/PostCardCell/PostCardMediaStack.swift
@@ -170,9 +170,10 @@ final class PostCardMediaStack: UIView {
                 
                 var blurImage: UIImage? = nil
                 if let blurhash = attachment.blurhash, imageFromCache == nil, let currentMedia = self.media, attachment.url != currentMedia.url {
-                    let blurWidth = attachment.meta?.original?.width != nil ? attachment.meta!.original!.width! / 20 : 32
-                    let blurHeight = attachment.meta?.original?.height != nil ? attachment.meta!.original!.height! / 20 : 32
-                    blurImage = UnifiedImage(blurHash: blurhash, size: .init(width: blurWidth, height: blurHeight))
+                    let blurWidth = attachment.meta?.small?.width ?? 32
+                    let blurHeight = attachment.meta?.small?.height ?? 32
+                    blurImage = UnifiedImage(blurHash: blurhash, size: .init(width: 32, height: 32))?
+                        .resized(to: .init(width: blurWidth, height: blurHeight))
                 }
                 photo.underlyingImage = imageFromCache ?? previewFromCache ?? blurImage
                 return photo
