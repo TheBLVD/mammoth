e409f1c3fd3c626053f4cab24a4d5dcea8e6261d
Benoit Nolens <bnolens@gmail.com>
2024-03-21 17:40:45 +0100

    MAM-4059-ghost-interactions-after-a-post-has-merged-in-remote-data

--- Mammoth/Screens/HomeScreen/NewsFeedViewModel.swift
+++ Mammoth/Screens/HomeScreen/NewsFeedViewModel.swift
@@ -544,28 +544,25 @@ extension NewsFeedViewModel {
 private extension NewsFeedViewModel {
     
     @objc func onPostCardUpdate(notification: Notification) {
-        DispatchQueue.main.async { [weak self] in
-            guard let self else { return }
-            if let postCard = notification.userInfo?["postCard"] as? PostCardModel {
-                if let isDeleted = notification.userInfo?["deleted"] as? Bool, isDeleted == true {
-                    // Delete post card data in list data and data source
-                    self.remove(card: postCard, forType: self.type)
-                } else {
-                    // Replace activity data in list that include this post card
-                    if NewsFeedTypes.allActivityTypes.contains(self.type) {
-                        self.listData.activity.forEach { (key, activities) in
-                            if let index = activities.firstIndex(where: {$0.extractPostCard()?.uniqueId == postCard.uniqueId}) {
-                                if case .activity(let activity) = activities[index] {
-                                    activity.postCard = postCard
-                                    let activityType: NotificationType? = key == "all" ? nil : NotificationType(rawValue: key)
-                                    self.update(with: .activity(activity), forType: .activity(activityType))
-                                }
+        if let postCard = notification.userInfo?["postCard"] as? PostCardModel {
+            if let isDeleted = notification.userInfo?["deleted"] as? Bool, isDeleted == true {
+                // Delete post card data in list data and data source
+                self.remove(card: postCard, forType: self.type)
+            } else {
+                // Replace activity data in list that include this post card
+                if NewsFeedTypes.allActivityTypes.contains(self.type) {
+                    self.listData.activity.forEach { (key, activities) in
+                        if let index = activities.firstIndex(where: {$0.extractPostCard()?.uniqueId == postCard.uniqueId}) {
+                            if case .activity(let activity) = activities[index] {
+                                activity.postCard = postCard
+                                let activityType: NotificationType? = key == "all" ? nil : NotificationType(rawValue: key)
+                                self.update(with: .activity(activity), forType: .activity(activityType))
                             }
                         }
-                    } else {
-                        // Replace post card data in list data and data source
-                        self.update(with: .postCard(postCard), forType: self.type)
                     }
+                } else {
+                    // Replace post card data in list data and data source
+                    self.update(with: .postCard(postCard), forType: self.type)
                 }
             }
         }
