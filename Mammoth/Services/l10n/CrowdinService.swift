e7a3f1662bca631c71b18d294eecaf80cea7a1ad
Benoit Nolens <bnolens@gmail.com>
2024-04-10 18:36:54 +0200

    MAM-4108-make-instances-list-sorting-work-of-pt-br

--- Mammoth/Services/l10n/CrowdinService.swift
+++ Mammoth/Services/l10n/CrowdinService.swift
@@ -22,6 +22,27 @@ struct l10n {
         CrowdinSDK.startWithConfig(crowdinSDKConfig, completion: { })
     }
     
+    public static func isCurrentLanguageSupported() -> Bool {
+        let supported = GlobalStruct.supportedLocalizations
+        if let currentLocale = self.getCurrentLocale() {
+            if !supported.contains(currentLocale) {
+                if let range = currentLocale.range(of: "-") {
+                    let languageCode = String(currentLocale[currentLocale.startIndex..<range.lowerBound])
+                    if supported.contains(languageCode) {
+                        return true
+                    }
+                }
+                
+                return false
+                
+            } else {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
     public static func checkForSupportedLanguage() {
         // Fallback to root localization if current device language is not supported
         let supported = GlobalStruct.supportedLocalizations
